2a2bb19dcf942c903be0f51409b4d4373e7fa32a
hagonator <letzighagen@gmail.com>
2024-12-08 16:14:17 +0100

    implemented sequential animations

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Project on Asynchronous JavaScript
 
---- Assessment from MDN's JavaScript curriculum ---
+--- Assessment from MDN's JavaScript curriculum --- 
+--- https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Sequencing_animations ---
+
--- static/js/main.js
+++ static/js/main.js
@@ -12,3 +12,14 @@ const aliceTiming = {
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
+
+alice1.priority = Math.random();
+alice2.priority = Math.random();
+alice3.priority = Math.random();
+
+let aliceList = [alice1, alice2, alice3];
+aliceList.sort((a, b) => (b.priority - a.priority));
+
+aliceList[0].animate(aliceTumbling, aliceTiming).finished
+  .then((response) => aliceList[1].animate(aliceTumbling, aliceTiming).finished)
+  .then((response) => aliceList[2].animate(aliceTumbling, aliceTiming).finished);
